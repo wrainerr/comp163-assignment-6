833394d601de1295738b094795af679185e80ac9
Rayner Paulino-Payano <raynerpaulino2007@gmail.com>
2025-10-07 22:52:21 -0400

    Add name and address cleaning with string methods

--- rpaulinopayano_assignment_6.py
+++ rpaulinopayano_assignment_6.py
@@ -18,6 +18,37 @@ while True:
     # check for correct field count
     if user_input.count("|") == 3:
         contact_count += 1
+        separated_contact_info = user_input.split("|")
+
+        # name cleaning
+        name_full = separated_contact_info[0].strip().title()
+        name_parts = name_full.split()
+        
+        # separate first and last name
+        if len(name_parts) >= 2:
+            # combine all words except the last into the first name
+            first_name = " ".join(name_parts[:-1]) 
+            # the last word is the last name
+            last_name = name_parts[-1]
+        else:
+            first_name = name_parts[0] if name_parts else "n/a"
+            last_name = "n/a"
+
+        # address cleaning
+        raw_address = separated_contact_info[3].strip()
+        # use title and split
+        separated_address = raw_address.title().split()
+        final_address = []
+        
+        # smart state abbreviation handling
+        for part in separated_address:
+            # check length is 2 and is alpha then use upper
+            if len(part) == 2 and part.isalpha():
+                final_address.append(part.upper())
+            else:
+                final_address.append(part)
+        # reassemble address using join
+        address = " ".join(final_address)
 
 # start printing output sections
 if cleaned_contacts_list:
